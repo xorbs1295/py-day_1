2daf48457733618d8e5f6ff3d1151ecb16f1fd85
xorbs1295 <xorbs1295@gmail.com>
2021-12-23 17:28:47 +0900

    day_1py

---
+++ day_1.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # python
+# ### 기본 문법 1일차 
+
+# In[1]:
+
+
+print('hello')
+print(10*20)
+10+20
+
+
+# In[2]:
+
+
+# 키워드
+import keyword
+print(keyword.kwlist)
+
+
+# In[3]:
+
+
+"hello" + ' test'
+
+
+# In[4]:
+
+
+print('test print')
+print(10+20)
+
+
+# In[5]:
+
+
+# print() 함수 사용해보기
+print('hello python!!')
+print("연산식 : ", 10+20)
+print("여러 자료 출력 : ", 10, 20, 'end')
+print() # 빈라인 출력
+print(10+20)
+print("test", "123", sep='#')
+
+
+# In[8]:
+
+
+#자료형 확인 
+print(type('문자열'))
+print(type(10))
+print(type(True))
+print(type(10.3))
+print(type('10'))
+
+
+# In[ ]:
+
+
+
+
